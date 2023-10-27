913671244da1638fbe34e1437f5cf102bb2605ab
jastach <jakub.stachowicz@nokia.com>
2023-10-27 09:31:09 +0200

    Connect with database

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,10 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from profiles.models import Item
+from .serializers import ItemSerializer
 
 @api_view(['GET'])
 def get_data(request):
     person = {'name': 'Kuba', 'age': 22}
+    item = Item("Siema")
     return Response(person)
