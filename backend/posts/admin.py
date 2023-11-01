0aa1c9355025c30c5ac17befcdbab1d3166f2c7b
mikitomi21 <kubastach2001@gmail.com>
2023-11-01 12:36:26 +0100

    Create comment model

--- backend/posts/admin.py
+++ backend/posts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 @admin.register(Post)
@@ -7,3 +7,10 @@ class AdminPost(admin.ModelAdmin):
     fields = ['author', 'title', 'text', 'created']
     list_display = fields
     list_filter = ['author', 'title']
+
+
+@admin.register(Comment)
+class AdminComment(admin.ModelAdmin):
+    fields = ['author', 'post', 'text', 'created', 'updated']
+    list_display = fields
+    list_filter = ['author', 'post']
