9f6b3ffbfcfc91f5ade7ee829d070757dd248811
Jakub Stachowicz <43847131+mikitomi21@users.noreply.github.com>
2023-11-03 08:56:13 +0100

    Create comment model (#3)
    * Create comment model
    
    * Fix security bugs
    
    * Add references for comments to post

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -22,11 +22,12 @@ from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from posts.viewsets import PostViewSet
+from posts.viewsets import PostViewSet, CommentViewSet
 
 router = DefaultRouter()
 
 router.register(r'posts', PostViewSet)
+router.register(r'comments', CommentViewSet)
 
 schema_view = get_schema_view(
    openapi.Info(
--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -1,12 +1,21 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Comment
 from authentication.serializers import CustomUserSerializer
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = CustomUserSerializer(read_only=True)
+    post = serializers.PrimaryKeyRelatedField(queryset=Post.objects.all())
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'post', 'text')
 
 
 class PostSerializer(serializers.ModelSerializer):
     author = CustomUserSerializer(read_only=True)
+    comments = CommentSerializer(read_only=True, many=True)
 
     class Meta:
         model = Post
-        fields = ('author', 'title', 'text')
-        read_only_fields = ('author',)
+        fields = ('author', 'title', 'text', 'comments')
