c3d90a9abaa336fa861d3abcfaf127908ba3e817
marcinstenka <mastenka@wp.pl>
2023-11-04 17:57:56 +0100

    posts from api in progress

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -179,3 +180,4 @@ DJOSER = {
 
     }
 }
+CORS_ALLOW_ALL_ORIGINS = True
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,16 @@
 'use client';
 import Image from 'next/image';
 import styles from './styles/page.module.scss';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import useSWR from 'swr'
+
+const fetcher = (url, auth) => {
+	console.log(auth)
+	fetch(url, {headers:{'Authorization' : `Token ${auth}`}}).then(res => res.json())}
 
 export default function Home() {
+	
+	
 	const [posts, setPosts] = useState([
 		{
 			user: 'Marcin Stenka',
@@ -21,12 +28,43 @@ export default function Home() {
 				'In semper cursus leo, ut facilisis urna rutrum quis. Vestibulum feugiat laoreet arcu, non suscipit quam mattis et. Nulla aliquet semper risus, non congue nunc posuere id. Fusce ultrices, lorem quis viverra pulvinar, odio ex volutpat elit, non iaculis arcu magna id augue. Curabitur cursus ipsum nisl. ',
 		},
 	]);
+	
+	
+	const [auth, setAuth] = useState('')
+	
+
+	useEffect(()=>{
+		const getAuthToken = async () => {
+			let user = {
+				"password": "password",
+				"email": "admin@example.com"
+			}
+			
+			const response = await fetch('http://localhost:8000/api/auth/token/login', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				  },
+				body: JSON.stringify(user),
+			});
+			const data = await response.json();
+			setAuth(data.auth_token);
+		}
+		getAuthToken();
+
+	}, [])
+	
+	const { data, error } = useSWR('http://localhost:8000/api/posts', () =>fetcher('http://localhost:8000/api/posts', auth))
+		
+	if (error) return <div style={{color:"white"}}>Failed to load</div>
+	if (!data) return <div style={{color:"white"}}>Loading...</div>
+	console.log(data)
 	return (
 		<>
 			<div className={styles.posts}>
 				{posts.map(post => {
 					return (
-						<div className={styles.post}>
+						<div className={styles.post} key={post.user}>
 							<div className={styles.user}>
 								<div className={styles.user_avatar}></div>
 								<h3 className={styles.user_name}>{post.user}</h3>
