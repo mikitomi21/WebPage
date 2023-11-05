761007b7f6368a7c02b98fb91890a312bf24c4ea
mikitomi21 <kubastach2001@gmail.com>
2023-11-05 16:02:22 +0100

    Add adding new friends

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     avatar = models.ImageField(upload_to='avatars', null=True, blank=True)
+    friends = models.ManyToManyField("self", blank=True, related_name="user_friends")
 
     def __str__(self):
         return self.username
--- backend/posts/viewsets.py
+++ backend/posts/viewsets.py
@@ -1,7 +1,10 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
-from .models import Post, Comment
+from .models import Post, Comment, CustomUser
 from .serializers import PostSerializer, CommentSerializer
-
+from rest_framework.response import Response
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
@@ -19,3 +22,16 @@ class CommentViewSet(ModelViewSet):
         author = self.request.user
         post = serializer.validated_data.get('post')
         serializer.save(author=author, post=post)
+
+    @action(detail=True, methods=['POST'])
+    def add_friend(self, request, pk):
+        user = request.user
+        friend = get_object_or_404(CustomUser, id=pk)
+
+        if not user.friends.filter(id=user.id).exists():
+            user.add_like(friend)
+            friend.add_like(user)
+            return Response({"message": f"{user} and {friend} are new friends"}, status=status.HTTP_200_OK)
+        return Response({"error": f"Can't connect {user} and {friend}, because they are friends"}, status=status.HTTP_400_BAD_REQUEST)
+
+
