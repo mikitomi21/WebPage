24cfe0da1ecd20c7aa8b71fc24a9fa58dfc34b76
mikitomi21 <kubastach2001@gmail.com>
2023-11-07 15:11:56 +0100

    Add commends

---
+++ backend/common/management/commands/cleardatabase.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from authentication.models import CustomUser
+from posts.models import Post, Comment
+
+class Command(BaseCommand):
+    help = "Delete of data from database"
+
+    def handle(self, *args, **options):
+        self.stdout.write("Deleting data from database...")
+        CustomUser.objects.all().delete()
+        Post.objects.all().delete()
+        Comment.objects.all().delete()
+        self.stdout.write("Deleted all data from database.")
