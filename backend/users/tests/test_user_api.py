2dcb0a39653262c21f05fda382d2b2113346bee3
Tim Schopinski <91907758+timschopinski@users.noreply.github.com>
2023-12-24 13:03:31 +0100

    add set password test

--- backend/users/tests/test_user_api.py
+++ backend/users/tests/test_user_api.py
@@ -4,6 +4,7 @@ from rest_framework.test import APITestCase
 from rest_framework.authtoken.models import Token
 from users.factories import UserFactory
 from users.models import CustomUser
+from common.test_utils import login_user
 
 
 class TestCustomUserAPI(APITestCase):
@@ -12,14 +13,11 @@ class TestCustomUserAPI(APITestCase):
         self.user1 = UserFactory()
         self.user2 = UserFactory()
 
-        self.token_user1 = Token.objects.create(user=self.user1)
-        self.token_user2 = Token.objects.create(user=self.user2)
-
     def authenticate(self, user):
         self.client.credentials(HTTP_AUTHORIZATION=f'Token {Token.objects.get(user=user).key}')
 
     def test_list_users(self):
-        self.authenticate(self.user1)
+        login_user(self.client, self.user1)
         response = self.client.get(reverse('customuser-list'))
         data = response.json()
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -27,14 +25,14 @@ class TestCustomUserAPI(APITestCase):
 
     def test_retrieve_user(self):
         detail_url = reverse('customuser-detail', args=[self.user1.id])
-        self.authenticate(self.user1)
+        login_user(self.client, self.user1)
         response = self.client.get(detail_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['id'], self.user1.id)
 
     def test_add_friend(self):
         add_friend_url = reverse('customuser-add-friend', args=[self.user2.id])
-        self.authenticate(self.user1)
+        login_user(self.client, self.user1)
         response = self.client.post(add_friend_url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['message'], f"{self.user1} and {self.user2} are new friends")
@@ -44,16 +42,15 @@ class TestCustomUserAPI(APITestCase):
 
     def test_add_self_as_friend(self):
         add_friend_url = reverse('customuser-add-friend', args=[self.user1.id])
-        self.authenticate(self.user1)
+        login_user(self.client, self.user1)
         response = self.client.post(add_friend_url, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data['error'], f"{self.user1} can't add yourself to friends")
 
     def test_add_existing_friend(self):
         self.user1.add_friend(self.user2)
-        add_friend_url = reverse('customuser-add-friend', args=[self.user1.id])
-
-        self.authenticate(self.user1)
+        add_friend_url = reverse('customuser-add-friend', args=[self.user2.id])
+        login_user(self.client, self.user1)
         response = self.client.post(add_friend_url, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(
---
+++ backend/users/tests/test_user_set_password.py
@@ -0,0 +1,84 @@
+from django.conf import settings
+from django.test.utils import override_settings
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
+from common.test_utils import login_user
+from users.factories import UserFactory
+
+
+class SetPasswordViewTest(APITestCase):
+    def setUp(self):
+        self.base_url = reverse("customuser-set-password")
+        self.current_password = 'secret12345'
+        self.user = UserFactory(password=self.current_password)
+
+    def test_post_set_new_password(self):
+        data = {"new_password": "new password", "current_password": self.current_password}
+        login_user(self.client, self.user)
+        response = self.client.post(self.base_url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.check_password(data["new_password"]))
+
+    @override_settings(DJOSER=dict(settings.DJOSER, **{"SET_PASSWORD_RETYPE": True}))
+    def test_post_set_new_password_with_retype(self):
+        data = {
+            "new_password": "new password",
+            "re_new_password": "new password",
+            "current_password": self.current_password,
+        }
+        login_user(self.client, self.user)
+
+        response = self.client.post(self.base_url, data)
+        self.assertTrue(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.check_password(data["new_password"]))
+
+    def test_post_not_set_new_password_if_wrong_current_password(self):
+        data = {"new_password": "new password", "current_password": "wrong"}
+        login_user(self.client, self.user)
+
+        response = self.client.post(self.base_url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    @override_settings(DJOSER=dict(settings.DJOSER, **{"SET_PASSWORD_RETYPE": True}))
+    def test_post_not_set_new_password_if_mismatch(self):
+        data = {
+            "new_password": "new password",
+            "re_new_password": "wrong",
+            "current_password": self.current_password,
+        }
+        login_user(self.client, self.user)
+
+        response = self.client.post(self.base_url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.check_password(data["current_password"]))
+
+    def test_post_not_set_new_password_if_fails_validation(self):
+        data = {
+            "new_password": "666",
+            "re_new_password": "666",
+            "current_password": self.current_password,
+        }
+        login_user(self.client, self.user)
+
+        expected_error_messages = [
+            'This password is too short. It must contain at least 10 characters.',
+            'This password is too common.',
+            'This password is entirely numeric.'
+        ]
+
+        response = self.client.post(self.base_url, data)
+        actual_error_messages = response.json().get('new_password', [])
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        for expected_error_message in expected_error_messages:
+            self.assertIn(expected_error_message, actual_error_messages)
+
+        self.assertEqual(len(actual_error_messages), len(expected_error_messages))
+        self.user.refresh_from_db()
+        self.assertFalse(self.user.check_password(data["current_password"]))
