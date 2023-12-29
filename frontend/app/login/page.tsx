ce1fa7923a66d34fb58606314f7de0905fe50586
marcinstenka <mastenka@wp.pl>
2023-12-29 12:28:23 +0100

    login page desgin

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -2,18 +2,34 @@ import styles from './page.module.scss';
 
 export default function Login() {
 	return (
-		<section className={styles.login}>
+		<section className={styles.container}>
 			<h3>Zaloguj się</h3>
 			<form className={styles.form}>
-				<div className={styles.form_separator}>
+				<div className={styles.form_field}>
 					<label htmlFor='userName'>Nazwa użytkownika</label>
-					<input type='text' name='userName' id='userName' />
+					<div className={styles.input_container}>
+						<input
+							className={styles.input}
+							type='text'
+							name='userName'
+							id='userName'
+						/>
+						<div className={styles.input_focus}></div>
+					</div>
 				</div>
-				<div className={styles.form_separator}>
+				<div className={styles.form_field}>
 					<label htmlFor='password'>Hasło</label>
-					<input type='password' name='password' id='password' />
+					<div className={styles.input_container}>
+						<input
+							className={styles.input}
+							type='password'
+							name='password'
+							id='password'
+						/>
+						<div className={styles.input_focus}></div>
+					</div>
 				</div>
-				<button>Zaloguj</button>
+				<button></button>
 			</form>
 		</section>
 	);
