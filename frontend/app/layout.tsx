f291db508eb7d8384a90d19eae028a9edd364883
marcinstenka <mastenka@wp.pl>
2024-01-05 10:55:25 +0100

    footer added

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.scss';
 import TokenContextProvider from './lib/context/token-context';
+import Footer from './lib/components/global/Footer';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -18,6 +19,7 @@ export default function RootLayout({
 		<html lang='pl'>
 			<body className={inter.className}>
 				<TokenContextProvider>{children}</TokenContextProvider>
+				<Footer />
 			</body>
 		</html>
 	);
