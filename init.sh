fa9e921e4ad180fd4c89747e13e9622c73b50824
Tim Schopinski <91907758+timschopinski@users.noreply.github.com>
2024-01-14 14:30:06 +0100

    add db volume

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+PROJECT_DIR=$(git rev-parse --show-toplevel)
+ROOT=$(basename "${PROJECT_DIR}")
+PROJECT_NAME=app
+
 running_containers=$(docker ps -f name=sharespace_* -aq)
 
 if [[ $running_containers ]]; then
@@ -9,6 +13,17 @@ if [[ $running_containers ]]; then
     echo "Removing stopped containers..."
     docker rm $running_containers
 fi
+
+
+echo "Removing unused containers (this may take a while)..."
+docker container prune --force
+
+echo "Removing docker volumes..."
+volumes="${ROOT}_postgres_data"
+
+docker volume rm -f ${volumes}
+
+sleep 10
 
 echo "Executing makemigrations..."
 docker compose run --rm django makemigrations
